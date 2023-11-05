5fe3e79f8bc8b967ca04b93f2ef96874f0a1047a
Gravtas-J <144185093+Gravtas-J@users.noreply.github.com>
2023-11-05 13:32:32 +1000

    Update Aut0 Example.py

--- Aut0 Example.py
+++ Aut0 Example.py
@@ -2,13 +2,14 @@ from auth0_component import login_button
 import streamlit as st
 from dotenv import load_dotenv
 import os
-import streamlit as st
+
+
 
 
 
 st.set_page_config(
     page_title=("Auth0 Example"),
-    initial_sidebar_state=st.session_state.sidebar_state
+    page_icon="🔐",
 )
 hide_st_style = """
             <style>
@@ -17,8 +18,8 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
 load_dotenv()
+
 clientId = os.environ['clientid']
 domain = os.environ['domainid']
 
@@ -31,11 +32,15 @@ def main():
 if 'logged_in' not in st.session_state:
     st.session_state.logged_in = False
 
+if 'user_id' not in st.session_state:
+    st.session_state.user_id = None
+
 # If user is not logged in, show the login button
 if not st.session_state.logged_in:
     user_info = login_button(clientId=clientId, domain=domain)
     if user_info:
         st.session_state.logged_in = True
+        st.session_state.user_id = user_info.get('sub', None)  # Assuming 'sub' is a unique identifier for the user
         st.rerun()
         main()
 elif st.session_state.user_id:
